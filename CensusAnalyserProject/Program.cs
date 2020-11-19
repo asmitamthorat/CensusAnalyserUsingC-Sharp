b40c839c050d9b8fe07d0bb7859e547d7a02fc09
asmitamthorat <asmita26296@gmail.com>
2020-11-19 06:53:41 +0530

    Welcome to Census Analyser Project

---
+++ CensusAnalyserProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CensusAnalyserProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Census Analyser Project");
+        }
+    }
+}
