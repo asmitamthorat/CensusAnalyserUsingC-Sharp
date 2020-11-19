ce63cb1c8593fde580c2bca69a9e93e4299644cb
asmitamthorat <asmita26296@gmail.com>
2020-11-19 08:23:12 +0530

    ADDED:loading the StateCensus csv file

---
+++ CensusAnalyserProject/StateCensusAnalyser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CensusAnalyserProject
+{
+   public class StateCensusAnalyser
+    {
+        public List<StateCensusDataDAO> StateCensusAnalyserlist = new List<StateCensusDataDAO>();
+        public Dictionary<String, List<StateCensusDataDAO>> Dictionary = new Dictionary<string, List<StateCensusDataDAO>>();
+        public List<StateCensusDataDAO> loadStateCensusData(String path)
+        {
+
+            var file = new System.IO.StreamReader(path);
+            StateCensusAnalyserlist = new CsvHelper.CsvReader(file, System.Globalization.CultureInfo.InvariantCulture)
+                    .GetRecords<StateCensusDataDAO>().ToList();
+
+            Dictionary.Add("StateCensusAnalyzer", StateCensusAnalyserlist);
+            var matchKey = "StateCensusAnalyzer";
+
+
+            return Dictionary[matchKey];
+        }
+    }
+}
